aace9a4211b18a7be6dc88fe961e35f6f96d3237
moujianghui <allen_mjh@163.com>
2022-07-18 15:22:01 +0800

    init project

---
+++ JHDemo/JHDemo/WelcomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.h
+//  JHDemo
+//
+//  Created by eastsoft_mjh on 2022/7/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WelcomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JHDemo/JHDemo/WelcomeViewController.m
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.m
+//  JHDemo
+//
+//  Created by eastsoft_mjh on 2022/7/18.
+//
+
+#import "WelcomeViewController.h"
+
+@interface WelcomeViewController ()
+
+@end
+
+@implementation WelcomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
